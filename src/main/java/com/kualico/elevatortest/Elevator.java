1304a780c1ab4780590fa8a65216aba7e0a48959
Rob Weaver <rob@accuweaver.com>
2015-05-20 17:14:16 -0600

    Update to include tests for requirements 4 and 5.

--- src/main/java/com/kualico/elevatortest/Elevator.java
+++ src/main/java/com/kualico/elevatortest/Elevator.java
@@ -1,34 +1,43 @@
 package com.kualico.elevatortest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author robweaver
  */
 public class Elevator {
-    
+
     private int numFloors;
     private int currentFloor;
+    private boolean doorOpen;
+    private List<Integer> stops;
+
     private boolean goingUp;
     private boolean goingDown;
 
     /**
      *
      */
-    public Elevator(){
+    public Elevator() {
         numFloors = 1;
-        currentFloor = 0;
+        currentFloor = 1;
+        stops = new ArrayList<>();
+        goingUp = goingDown = false;
     }
-    
+
     /**
      *
      * @param numFloors
      */
-    public Elevator(int numFloors){
-        super();
-        if (numFloors > 1){
+    public Elevator(int numFloors) {
+        this();
+        if (numFloors > 1) {
             this.numFloors = numFloors;
         }
     }
+
     /**
      * @return the currentFloor
      */
@@ -37,11 +46,47 @@ public class Elevator {
     }
 
     /**
+     *
+     * @return
+     */
+    public boolean isDoorOpen() {
+        return doorOpen;
+    }
+
+    /**
+     *
+     * @param doorOpen
+     */
+    public void setDoorOpen(boolean doorOpen) {
+        this.doorOpen = doorOpen;
+    }
+
+    /**
+     * Tell us what floor we will be at on next move.
+     *
+     * @return
+     */
+    public int getNextFloor() {
+        if (isGoingUp()) {
+            return nextFloorUp();
+        }
+        if (isGoingDown()) {
+            return nextFloorDown();
+        }
+        return currentFloor;
+    }
+
+    /**
      * @param currentFloor the currentFloor to set
      */
     public void setCurrentFloor(int currentFloor) {
-        int oldCurrentFloor = this.currentFloor;
-        this.currentFloor = currentFloor;
+        if (currentFloor < 1) {
+            this.currentFloor = 1;
+        } else {
+            if (currentFloor <= numFloors) {
+                this.currentFloor = currentFloor;
+            }
+        }
     }
 
     /**
@@ -55,11 +100,12 @@ public class Elevator {
      * @param goingUp the goingUp to set
      */
     public void setGoingUp(boolean goingUp) {
-        boolean oldGoingUp = this.goingUp;
         this.goingUp = goingUp;
     }
 
     /**
+     * Set the
+     *
      * @return the goingDown
      */
     public boolean isGoingDown() {
@@ -67,14 +113,17 @@ public class Elevator {
     }
 
     /**
+     * Set the value as going down
+     *
      * @param goingDown the goingDown to set
      */
     public void setGoingDown(boolean goingDown) {
-        boolean oldGoingDown = this.goingDown;
         this.goingDown = goingDown;
     }
 
     /**
+     * Get the number of floors
+     *
      * @return the numFloors
      */
     public int getNumFloors() {
@@ -82,11 +131,108 @@ public class Elevator {
     }
 
     /**
+     * Set the number of floors.
+     *
      * @param numFloors the numFloors to set
      */
     public void setNumFloors(int numFloors) {
         this.numFloors = numFloors;
     }
-    
-    
+
+    /**
+     * Return which floor would be next up
+     *
+     * @return
+     */
+    private int nextFloorUp() {
+        if (currentFloor < numFloors) {
+            return currentFloor + 1;
+        }
+        return currentFloor;
+    }
+
+    /**
+     * Return which floor would be next down
+     *
+     * @return
+     */
+    private int nextFloorDown() {
+        if (currentFloor > 1) {
+            return currentFloor - 1;
+        }
+        return currentFloor;
+    }
+
+    /**
+     * Upward
+     *
+     * @return
+     */
+    public int upward() {
+        currentFloor = nextFloorUp();
+        if (currentFloor == numFloors) {
+            setGoingUp(false);
+        }
+        setDoorOpen(isStop(currentFloor));
+        return currentFloor;
+    }
+
+    /**
+     * Downward
+     *
+     * @return
+     */
+    public int downward() {
+        currentFloor = nextFloorDown();
+        if (currentFloor == 1) {
+            setGoingDown(false);
+        }
+        setDoorOpen(isStop(currentFloor));
+        return currentFloor;
+    }
+
+    /**
+     *
+     * @param currentFloor
+     * @return
+     */
+    public boolean isStop(int currentFloor) {
+        boolean stopFlag = getStops().contains(currentFloor);
+        return stopFlag;
+    }
+
+    /**
+     * @return the stops
+     */
+    public List<Integer> getStops() {
+        return stops;
+    }
+
+    /**
+     * @param stops the stops to set
+     */
+    public void setStops(List<Integer> stops) {
+        this.stops = stops;
+    }
+
+    /**
+     * Add stop
+     *
+     * @param floor
+     */
+    public void addStop(int floor) {
+        stops.add(floor);
+    }
+
+    /**
+     * Remove stop
+     *
+     * @param floor
+     */
+    public void removeStop(int floor) {
+        if (stops.contains(floor)) {
+            stops.remove(floor);
+        }
+    }
+
 }
--- src/test/java/com/kualico/elevatortest/ElevatorTest.java
+++ src/test/java/com/kualico/elevatortest/ElevatorTest.java
@@ -1,5 +1,7 @@
 package com.kualico.elevatortest;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -12,34 +14,37 @@ import static org.junit.Assert.*;
  * @author robweaver
  */
 public class ElevatorTest {
-    
+
+    private Elevator instance;
+
     /**
      *
      */
     public ElevatorTest() {
     }
-    
+
     /**
      *
      */
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     /**
      *
      */
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     /**
      *
      */
     @Before
     public void setUp() {
+        instance = new Elevator();
     }
-    
+
     /**
      *
      */
@@ -53,11 +58,10 @@ public class ElevatorTest {
     @Test
     public void testGetCurrentFloor() {
         System.out.println("getCurrentFloor");
-        Elevator instance = new Elevator();
-        int expResult = 0;
+        int expResult = 1;
         int result = instance.getCurrentFloor();
         assertEquals(expResult, result);
-        
+
     }
 
     /**
@@ -67,9 +71,8 @@ public class ElevatorTest {
     public void testSetCurrentFloor() {
         System.out.println("setCurrentFloor");
         int currentFloor = 0;
-        Elevator instance = new Elevator();
         instance.setCurrentFloor(currentFloor);
-       
+        assertEquals(1, instance.getCurrentFloor());
     }
 
     /**
@@ -78,11 +81,10 @@ public class ElevatorTest {
     @Test
     public void testIsGoingUp() {
         System.out.println("isGoingUp");
-        Elevator instance = new Elevator();
         boolean expResult = false;
         boolean result = instance.isGoingUp();
         assertEquals(expResult, result);
-        
+
     }
 
     /**
@@ -92,9 +94,8 @@ public class ElevatorTest {
     public void testSetGoingUp() {
         System.out.println("setGoingUp");
         boolean goingUp = false;
-        Elevator instance = new Elevator();
         instance.setGoingUp(goingUp);
-        
+
     }
 
     /**
@@ -103,11 +104,10 @@ public class ElevatorTest {
     @Test
     public void testIsGoingDown() {
         System.out.println("isGoingDown");
-        Elevator instance = new Elevator();
         boolean expResult = false;
         boolean result = instance.isGoingDown();
         assertEquals(expResult, result);
-       
+
     }
 
     /**
@@ -117,9 +117,166 @@ public class ElevatorTest {
     public void testSetGoingDown() {
         System.out.println("setGoingDown");
         boolean goingDown = false;
-        Elevator instance = new Elevator();
         instance.setGoingDown(goingDown);
-       
+
     }
-    
+
+    /**
+     * Test of isDoorOpen method, of class Elevator.
+     */
+    @Test
+    public void testIsDoorOpen() {
+        System.out.println("isDoorOpen");
+        boolean expResult = false;
+        boolean result = instance.isDoorOpen();
+        assertEquals(expResult, result);
+
+    }
+
+    /**
+     * Test of setDoorOpen method, of class Elevator.
+     */
+    @Test
+    public void testSetDoorOpen() {
+        System.out.println("setDoorOpen");
+        boolean doorOpen = false;
+        instance.setDoorOpen(doorOpen);
+
+    }
+
+    /**
+     * Test of getNextFloor method, of class Elevator.
+     */
+    @Test
+    public void testGetNextFloor() {
+        System.out.println("getNextFloor");
+        int expResult = 1;
+        int result = instance.getNextFloor();
+        assertEquals(expResult, result);
+
+    }
+
+    /**
+     * Test of getNumFloors method, of class Elevator.
+     */
+    @Test
+    public void testGetNumFloors() {
+        System.out.println("getNumFloors");
+        int expResult = 0;
+        int result = instance.getNumFloors();
+
+    }
+
+    /**
+     * Test of setNumFloors method, of class Elevator.
+     */
+    @Test
+    public void testSetNumFloors() {
+        System.out.println("setNumFloors");
+        int numFloors = 0;
+        instance.setNumFloors(numFloors);
+
+    }
+
+    /**
+     * Test of upward method, of class Elevator.
+     */
+    @Test
+    public void testUpward() {
+        System.out.println("upward");
+        int expResult = 1;
+        int result = instance.upward();
+        assertEquals(expResult, result);
+
+    }
+
+    /**
+     * Test of downward method, of class Elevator.
+     */
+    @Test
+    public void testDownward() {
+        System.out.println("downward");
+        int expResult = 1;
+        int result = instance.downward();
+        assertEquals(expResult, result);
+
+    }
+
+    /**
+     * Test of isStop method, of class Elevator.
+     */
+    @Test
+    public void testIsStop() {
+        System.out.println("isStop");
+        int currentFloor = 0;
+        boolean expResult = false;
+        boolean result = instance.isStop(currentFloor);
+        assertEquals(expResult, result);
+
+    }
+
+    /**
+     * Test of getStops method, of class Elevator.
+     */
+    @Test
+    public void testGetStops() {
+        System.out.println("getStops");
+        List<Integer> expResult = new ArrayList<>();
+        List<Integer> result = instance.getStops();
+        assertEquals(expResult, result);
+
+    }
+
+    /**
+     * Test of setStops method, of class Elevator.
+     */
+    @Test
+    public void testSetStops() {
+        System.out.println("setStops");
+        List<Integer> stops = null;
+        instance.setStops(stops);
+
+    }
+
+    /**
+     * Test of addStop method, of class Elevator.
+     */
+    @Test
+    public void testAddStop() {
+        System.out.println("addStop");
+        int floor = 0;
+        instance.addStop(floor);
+
+    }
+
+    /**
+     * Test of removeStop method, of class Elevator.
+     */
+    @Test
+    public void testRemoveStop() {
+        System.out.println("removeStop");
+        int floor = 0;
+        instance.removeStop(floor);
+
+    }
+
+    /**
+     * Test to verify going down doesn't go below 1.
+     */
+    @Test
+    public void testBelowGround() {
+        instance.setCurrentFloor(1);
+        assertEquals(1, instance.getCurrentFloor());
+        assertEquals(1, instance.getNextFloor());
+    }
+
+    /**
+     * Test to verify that going up doesn't go above top floor
+     */
+    @Test
+    public void testAboveTop() {
+        instance.setCurrentFloor(instance.getNumFloors() + 5);
+        assertEquals(instance.getNumFloors(), instance.getCurrentFloor());
+        assertEquals(instance.getNumFloors(), instance.getNextFloor());
+    }
 }
