7e59eca6e55ce61e4eba93eae723eac37e50414a
Rob Weaver <rob@accuweaver.com>
2015-05-20 15:46:35 -0600

    Initial setup with empty beans / classes

---
+++ src/main/java/com/kualico/elevatortest/Building.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kualico.elevatortest;
+
+import java.util.List;
+
+/**
+ *
+ * @author robweaver
+ */
+public class Building {
+    private List<Elevator> elevators;
+}
